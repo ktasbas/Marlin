8c89b5c03ce4ee3e82e8e1375c0acfaad2189916
Kaan Tasbas <mktasbas@gmail.com>
2020-07-01 00:14:07 -0400

    Configure E0 fan to operate automatically
    SKR 1.4 only has 1 controllable fan output (part fan), but E1 output pins can be used to control E0 fan instead since we only have 1 extruder anyway.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -420,7 +420,11 @@
  * Multiple extruders can be assigned to the same pin in which case
  * the fan will turn on when any selected extruder is above the threshold.
  */
-#define E0_AUTO_FAN_PIN -1
+#if HOTENDS == 1
+  #define E0_AUTO_FAN_PIN P2_04 // SKR 1.4 can use E1 pin to control a 2nd fan instead
+#else
+  #define E0_AUTO_FAN_PIN -1
+#endif
 #define E1_AUTO_FAN_PIN -1
 #define E2_AUTO_FAN_PIN -1
 #define E3_AUTO_FAN_PIN -1
